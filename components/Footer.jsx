0e1ab485e1450e47f8a4ba2ef29dcd0b015d9842
Soumajit <soumajit2764@gmail.com>
2025-06-26 12:01:05 +0530

    Initial commit

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -45,7 +45,7 @@ const Footer = () => {
         </div>
       </div>
       <p className="py-4 text-center text-xs md:text-sm">
-        Copyright 2025 © GreatStack.dev All Right Reserved.
+        Copyright 2025  All Right Reserved.
       </p>
     </footer>
   );
